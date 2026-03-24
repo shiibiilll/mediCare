67805f82e3c7dfca2138761b058a48dc1df61ead
shibil <shibil6331@gmail.com>
2026-03-24 18:27:57 +0530

    Finished Navbar

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,10 +1,44 @@
 import React from "react";
+import { Route, Routes, Link } from "react-router-dom";
+import Hero from "./pages/Hero";
+import { useUser } from "@clerk/react";
 
 const App = () => {
+  function RequireAuth({ children }) {
+    const { isLoaded, isSignedIn } = useUser();
+
+    if (!isLoaded) return null;
+    if (!isSignedIn)
+      return (
+        <div
+          className="min-h-screen font-mono flex items-center justify-center
+         bg-linear-to-b from-emarald-50 via-green-50 to-emerald-100 px-4"
+        >
+          <div className="text-center">
+            <p className="text-emerald-800 font-semibold text-lg sm:text-2xl mb-4 animate-fade-in">
+              Please sign in to view this page
+            </p>
+
+            <div className="flex justify-center">
+              <Link
+                to="/"
+                className="px-4 py-2 text-sm rounded-full bg-emerald-600 text-white
+                shadow-sm hover:bg-emerald-700 hover:shadow-md transition-all durartion-300
+                ease-in-out animate-bounce-subtle"
+              >
+                Home
+              </Link>
+            </div>
+          </div>
+        </div>
+      );
+    return children;
+  }
+
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<Hero />} />
+    </Routes>
   );
 };
 
