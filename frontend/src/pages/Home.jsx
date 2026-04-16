7c28af32e3551ac42d8cd7d538e67641e29d36cf
shibil <shibil6331@gmail.com>
2026-04-17 01:17:08 +0530

    Created a testimonial section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Navbar from "../components/Navbar.jsx";
 import Banner from "../components/Banner.jsx";
 import Certification from '../components/Certification.jsx';
 import HomeDoctors from "../components/HomeDoctors.jsx";
+import Testimonial from "../components/testimonial.jsx";
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
       <Banner />
       <Certification />
       <HomeDoctors />
+      <Testimonial />
     </div>
   );
 };
