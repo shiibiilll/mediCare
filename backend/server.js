52f4ae40f2ff2db7735862981fb7c411e62c13c1
shibil <shibil6331@gmail.com>
2026-04-29 00:26:33 +0530

    Allowd origins changed to render url from vite url

--- backend/server.js
+++ backend/server.js
@@ -15,8 +15,8 @@ const PORT = process.env.PORT || 3001;
 const allowedOrigins = [
   'http://localhost:3001',
   'http://localhost:3002',
-  'http://localhost:5173',
-  'http://localhost:5174',
+  'https://wellnessway-frontend.onrender.com',
+  'https://wellnessway-admin.onrender.com',
 ];
 
 // Middleware
